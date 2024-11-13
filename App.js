2a2bdeb1000e216ebe33a120c41a120091d7301d
Eric Wang <wang.yichen7@northeastern.edu>
2024-11-13 15:21:03 -0800

    refactor: extracts navigator file from App.js

--- App.js
+++ App.js
@@ -1,47 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import { colors } from './styles/globalStyles';
-
-
-import HomeScreen from './screens/HomeScreen';
-import BookmarksScreen from './screens/BookmarksScreen';
-import FootprintScreen from './screens/FootprintScreen';
-import AccountScreen from './screens/AccountScreen';
-
-const Tab = createBottomTabNavigator();
+import { AppNavigator } from './navigation/AppNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Bookmarks') {
-              iconName = focused ? 'bookmark' : 'bookmark-outline';
-            } else if (route.name === 'Footprint') {
-              iconName = focused ? 'footsteps' : 'footsteps-outline';
-            } else if (route.name === 'Account') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: colors.primary,
-          tabBarInactiveTintColor: colors.textSecondary,
-          headerShown: true,
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Bookmarks" component={BookmarksScreen} />
-        <Tab.Screen name="Footprint" component={FootprintScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
-      </Tab.Navigator>
+      <AppNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
